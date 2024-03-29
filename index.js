a9f50e46683e0799657f99ed922b7fc789a13736
riamuuarmi <shumailkhan44@gmail.com>
2024-03-29 22:30:58 +0500

    calculator task 1

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "firstNumber",
+        message: "Enter Your First Nubmer",
+    },
+    {
+        type: "number",
+        name: "secondNumber",
+        message: "Enter Your Second Nubmer",
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/"],
+        message: "Select The Operator",
+    },
+]);
+if (answers.operator === "+") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "-") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "*") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "/") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("you have Enter Invaild operator");
+}
